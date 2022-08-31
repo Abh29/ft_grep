7d59a8e79301b855b363e03f8c51b049ce9698d4
Abh29 <aboutifor@stud.kpfu.ru>
2022-09-01 00:58:34 +0300

    spliting the code to files

--- ft_grep.h
+++ ft_grep.h
@@ -11,6 +11,19 @@ typedef struct s_list
 }				t_list;
 #endif
 
+
+typedef struct s_grep
+{
+    t_list      *patterns_list;
+    t_list      *files;
+    t_list      *patterns_path;
+    char        *patterns;
+    int         flags;  //eivclnhsfoE
+    int         c_lines;
+    int         c_matches;
+}               t_grep;
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -22,6 +35,10 @@ typedef struct s_list
 #include <string.h>
 
 
+#define F_MULTFILE  200
+#define F_EMPTLINE  201
+#define F_COLORED   300
+
 
 #define S21_WHITE   "\x1b[0m"
 #define S21_RED     "\x1b[1;91m" // Bold Red
@@ -31,7 +48,7 @@ typedef struct s_list
 #define S21_MAGENTA "\x1b[35m"
 #define S21_CYAN    "\x1b[36m"
 
-
+extern int exit_status;
 
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstnew(void *content);
@@ -48,6 +65,27 @@ char	*ft_strchr(const char *s, int c);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_base_fd(int nb, const char *base, int fd);
 void	*ft_memset(void *b, int c, size_t len);
+void    exit_perror(char *msg, int status, int mask);
+void    *ft_allocate(size_t n);
+void    exit_msg(char *msg, int status, int mask);
+void    print_error(char *msg, int status, int mask);
+int     check_flag(t_grep *arg, int flag);
+void    set_flag(t_grep *arg, int flag);
+void    init_grep(t_grep *arg);
+void    read_args(t_grep *arg, int argc, char **argv);
+void    split_patterns(t_grep *arg);
+void    compile_patterns(t_grep *arg);
+int     match(t_grep *arg, char *str, regmatch_t pmatch[], t_list **matches);
+void    print_prifix(t_grep *arg, char *str, char *file_path);
+void   	print_matches_lines(t_grep *arg, char *str, t_list *matches, char *file_path);
+void    print_matches_clean(t_grep *arg, char *str, t_list *matches, char *file_path);
+int    	match_file(t_grep *arg, char *file_path);
+void    match_files_list(t_grep *arg);
+void    free_split(char ***arr);
+void    free_reg(void *re);
+void    free_args(t_grep *arg);
+void    print_args(t_grep *arg);
+
 
 
 
